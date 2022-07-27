9bbc49e36329ef49b3d427a8f5016fcb8b337191
akyoscommunication <50996280+akyoscommunication@users.noreply.github.com>
2022-07-27 14:02:59 +0200

    Update CmsBundleAuthenticator for SF6 & PHP8

--- Security/CmsBundleAuthenticator.php
+++ Security/CmsBundleAuthenticator.php
@@ -2,95 +2,51 @@
 
 namespace Akyos\CmsBundle\Security;
 
-use Akyos\CmsBundle\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
-use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-// TODO => Refaire les authenticator, ça a tout changé...
-class CmsBundleAuthenticator extends AbstractFormLoginAuthenticator
+class CmsBundleAuthenticator extends AbstractLoginFormAuthenticator
 {
-	use TargetPathTrait;
+    use TargetPathTrait;
 
-	private EntityManagerInterface $entityManager;
-	private UrlGeneratorInterface $urlGenerator;
-	private CsrfTokenManagerInterface $csrfTokenManager;
-	private UserPasswordHasherInterface $passwordHasher;
+    public function __construct(
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly CsrfTokenManagerInterface $csrfTokenManager,
+    ) {}
 
-	public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator, CsrfTokenManagerInterface $csrfTokenManager, UserPasswordHasherInterface $passwordHasher)
-	{
-		$this->entityManager = $entityManager;
-		$this->urlGenerator = $urlGenerator;
-		$this->csrfTokenManager = $csrfTokenManager;
-		$this->passwordHasher = $passwordHasher;
-	}
-
-	public function supports(Request $request): bool
+    protected function getLoginUrl(Request $request): string
     {
-		return 'app_login' === $request->attributes->get('_route')
-			&& $request->isMethod('POST');
-	}
+        return $this->urlGenerator->generate('app_login');
+    }
 
-	public function getCredentials(Request $request): array
+    public function authenticate(Request $request): Passport
     {
-		$credentials = [
-			'email' => $request->request->get('email'),
-			'password' => $request->request->get('password'),
-			'csrf_token' => $request->request->get('_csrf_token'),
-		];
-		$request->getSession()->set(
-			Security::LAST_USERNAME,
-			$credentials['email']
-		);
+        $username = $request->request->get('email');
+        $password = $request->request->get('password');
+        $csrfToken = $request->request->get('_csrf_token');
 
-		return $credentials;
-	}
+        return new Passport(
+            new UserBadge($username),
+            new PasswordCredentials($password),
+            [new CsrfTokenBadge('authenticate', $csrfToken)]
+        );
+    }
 
-	public function getUser($credentials, UserProviderInterface $userProvider)
-	{
-		$token = new CsrfToken('authenticate', $credentials['csrf_token']);
-		if (!$this->csrfTokenManager->isTokenValid($token)) {
-			throw new InvalidCsrfTokenException('Jeton CSRF invalide.');
-		}
-
-		$user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $credentials['email']]);
-
-		if (!$user) {
-			// fail authentication with a custom error
-			throw new CustomUserMessageAuthenticationException('Cet email n\'est associé à aucun compte utilisateur.');
-		}
-
-		return $user;
-	}
-
-	public function checkCredentials($credentials, UserInterface $user): bool
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): RedirectResponse
     {
-		return $this->passwordHasher->isPasswordValid($user, $credentials['password']);
-	}
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
 
-	public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey): RedirectResponse
-    {
-		if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-			return new RedirectResponse($targetPath);
-		}
-
-		return new RedirectResponse($this->urlGenerator->generate('cms_index'));
-	}
-
-	protected function getLoginUrl(): string
-    {
-		return $this->urlGenerator->generate('app_login');
-	}
+        return new RedirectResponse($this->urlGenerator->generate('cms_index'));
+    }
 }
