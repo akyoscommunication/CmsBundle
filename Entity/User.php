407c285618b41f78dfeff3fb9719b08baf7d5b5b
akyoscommunication <50996280+akyoscommunication@users.noreply.github.com>
2022-11-24 14:21:58 +0100

    Fix user table name

--- Entity/User.php
+++ Entity/User.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Akyos\CmsBundle\Repository\UserRepository;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`User`')]
+#[ORM\Table(name: 'user')]
 class User extends BaseUser
 {
     public const ROLES = ['Visiteur' => 'ROLE_USER', 'Community Manager' => 'ROLE_CM', 'Administrateur' => 'ROLE_ADMIN', 'Super Admin' => 'ROLE_SUPER_ADMIN', 'Akyos' => 'ROLE_AKYOS'];
